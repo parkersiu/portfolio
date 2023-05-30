700397a5d6f3f70b1859afb7189ab3049a62c426
Parker Siu <parker.siu@gmail.com>
2023-05-30 14:29:36 -0700

    Added additional routes with skeleton for each page.

---
+++ app/airport-weather/page.js
@@ -0,0 +1,10 @@
+import Navbar from "@/app/components/navbar"
+
+export default function Page() {
+  return (
+    <>
+      <Navbar />
+      <div>Airport Weather page</div>
+    </>
+  );
+}
---
+++ app/milestone/page.js
@@ -0,0 +1,10 @@
+import Navbar from "@/app/components/navbar"
+
+export default function Page() {
+  return (
+    <>
+      <Navbar />
+      <div>Milestone page</div>
+    </>
+  );
+}
