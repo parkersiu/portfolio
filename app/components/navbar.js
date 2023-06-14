1701f9f6b657823b8fa9227dfe39c6451c892bdc
Parker Siu <parker.siu@gmail.com>
2023-06-14 11:11:33 -0700

    Reduced navbar padding.

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -25,7 +25,7 @@ export default function Navbar() {
 
   return (
     <header className="bg-gray-50 rounded-b-md border">
-      <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+      <nav className="mx-auto flex max-w-7xl items-center justify-between p-4 lg:px-8" aria-label="Global">
         <div className="flex lg:flex-1">
           <Link href="/" className="-m-1.5 p-1.5">
             <span className="sr-only">Parker Siu</span>
